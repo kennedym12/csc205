4321a7311bf006f6105601c09a6af6a9143e52b9
kennedym12 <k.mays0203@gmail.com>
2022-01-05 22:43:15 -0500

    completed the palindrome function

--- Practice.java
+++ Practice.java
@@ -20,8 +20,51 @@ public class Practice{
     }
 
     public static void main(String args[]){
-        String name = "Kennedy";
+        String name = "37073";
+        /*
         String result=reverseString(name);
         System.out.printf("%s is %s reversed",result,name);
+            */
+        //System.out.print(countVowels(name));
+
+        System.out.print(checkIfPalindrome(name));
+
     }
+
+    //takes a string and counts its vowels
+    public static int countVowels(String str){
+        int numVowels=0;
+        String compare;
+        String[] vowels = {"a","e","i","o","u"};
+        for(int i=0; i<str.length(); i++){
+            if(i!=str.length()-1){
+                compare=str.substring(i,i+1);
+            }
+            else{
+                compare=str.substring(i);
+            }
+            for(String v: vowels){
+                if(compare.equals(v)){
+                    numVowels++;
+                }
+            }
+        }
+        return numVowels;
+    }
+
+    //checks whether the word backwards is the same forward
+    public static boolean checkIfPalindrome(String word){
+        String backwards="";
+        for(int i=word.length()-1; i>=0; i--){
+            if(i!=word.length()-1){
+                backwards+=word.substring(i,i+1);
+            }
+            else{backwards+=word.substring(i);}
+        }
+        return(word.equals(backwards));
+    }
+
+
 }
+
+    
